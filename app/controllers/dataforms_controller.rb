2f1e361c0c659d0851f37b9e8d524bca46e7cb7d
purimph <purim@odds.team>
2024-09-05 17:45:39 +0700

    feat:create function register record to DB

--- app/controllers/dataforms_controller.rb
+++ app/controllers/dataforms_controller.rb
@@ -2,4 +2,20 @@ class DataformsController < ApplicationController
 
     def registerForm
     end
+
+    def create
+        @registration = Registration.new(param)
+
+        if @registration.save
+            redirect_to root_path
+        else
+            render :registerForm
+        end
+
+    end
+
+    private
+    def param
+        params.require(:registration).permit(:first_name, :last_name, :birthday, :gender, :email, :phone_number, :subject)
+    end
 end
